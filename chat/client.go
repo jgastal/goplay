ba07ef97329da2dd8a40fa181dad3761975a96a4
Jonas Gastal <gastal@noobfusion.com>
2014-05-17 12:57:56 -0300

    Server for chat over websockets.

---
+++ chat/client.go
@@ -0,0 +1,85 @@
+package chat
+
+import (
+	"github.com/gorilla/websocket"
+	"io"
+)
+
+type Client struct {
+	ws       *websocket.Conn
+	Username string
+	server   *Server
+	writeCh  chan *message
+	stopCh   chan bool
+}
+
+type message struct {
+	Method string `json:"method"`
+	Params interface{} `json:"params"`
+}
+
+func newClient(c *websocket.Conn, uname string, s *Server) *Client {
+	return &Client{
+		c,
+		uname,
+		s,
+		make(chan *message),
+		make(chan bool),
+	}
+}
+
+func (c *Client) Listen() {
+	go c.write()
+	for {
+		msg := new(message)
+		err := c.ws.ReadJSON(msg)
+		if err == io.EOF {
+			c.stopCh <- true
+			break
+		} else if err != nil {
+			continue
+		}
+
+		if msg.Method == "GetNicks" {
+			c.replyNicks()
+		} else if msg.Method == "Say" {
+			c.say(msg.Params.(string))
+		}
+	}
+	c.server.Del(c)
+}
+
+func (c *Client) write() {
+	for {
+		select {
+			case <- c.stopCh:
+				return
+			case msg := <- c.writeCh:
+				err := c.ws.WriteJSON(msg)
+				if err == io.EOF {
+					//Read will get it's own EOF soon enough
+					break
+				}
+		}
+	}
+}
+
+func (c *Client) replyNicks() {
+	c.writeCh <- &message{"Nicks", c.server.GetNicks()}
+}
+
+func (c *Client) say(msg string) {
+	c.server.Broadcast(c.Username, msg)
+}
+
+func (c *Client) NewMessage(who string, what string) {
+	c.writeCh <- &message{"NewMessage", []string{who, what}}
+}
+
+func (c *Client) Joined(who string) {
+	c.writeCh <- &message{"Joined", who}
+}
+
+func (c *Client) Left(who string) {
+	c.writeCh <- &message{"Left", who}
+}
---
+++ chat/server.go
@@ -0,0 +1,88 @@
+package chat
+
+import (
+	"github.com/gorilla/context"
+	"github.com/gorilla/websocket"
+	"github.com/jgastal/goplay/handlers"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{}
+
+type Server struct {
+	handler func(http.ResponseWriter, *http.Request)
+	clients map[string]*Client
+	addCh   chan *Client
+	delCh   chan *Client
+	name    string
+}
+
+func NewServer(name string) *Server {
+	s := Server{}
+	s.addCh = make(chan *Client)
+	s.delCh = make(chan *Client)
+	s.clients = make(map[string]*Client)
+	s.name = name
+
+	go s.run()
+	return &s 
+}
+
+func (s *Server) GetHandler() func(http.ResponseWriter, *http.Request) {
+	if s.handler == nil {
+		s.handler = func(w http.ResponseWriter, r *http.Request) {
+			u := context.Get(r, "username").(string)
+			ws, err := upgrader.Upgrade(w, r, nil)
+			if err != nil {
+				handlers.InternalErrorHandler(w, r)
+				return
+			}
+
+			client := newClient(ws, u, s)
+			s.addCh <- client
+			client.Listen()
+		}
+	}
+	return s.handler
+}
+
+func (s *Server) run() {
+	for {
+		select {
+			case c := <- s.addCh:
+				log.Println(c.Username, "connected")
+				for _, v := range s.clients {
+					v.Joined(c.Username)
+				}
+				s.clients[c.Username] = c
+
+			case c := <- s.delCh:
+				log.Println(c.Username, "disconnected")
+				delete(s.clients, c.Username)
+				for _, v := range s.clients {
+					v.Left(c.Username)
+				}
+		}
+	}
+}
+
+func (s *Server) Del(c *Client) {
+	s.delCh <- c
+}
+
+func (s *Server) GetNicks() []string {
+	reply := make([]string, len(s.clients))
+	i := 0
+	for k := range s.clients {
+		reply[i] = k
+		i++
+	}
+	return reply
+}
+
+func (s *Server) Broadcast(who string, what string) {
+	for _, v := range s.clients {
+		v.NewMessage(who, what)
+	}
+}
